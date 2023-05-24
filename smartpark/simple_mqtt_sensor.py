be07f7d6ff3831a9ef76f5b07d21a25339330c0f
TDM\avigar <rafael.avigad@nmtafe.wa.edu.au>
2023-05-24 16:43:41 +0800

    after mqtt is added

--- smartpark/simple_mqtt_sensor.py
+++ smartpark/simple_mqtt_sensor.py
@@ -1,14 +1,42 @@
+import paho.mqtt.client as paho
+
+
+
+
+
+# client.publish("lot/sensor", "Hello from Python")
+
+
 class Sensor:
     def __init__(self, config):
         self.name = config['name']
         self.location = config['location']
+        self.topic = config['topic']
+        self.broker = config['broker']
+        self.port = config['port']
+        self.client = paho.Client()
+        self.client.connect(self.broker,
+                            self.port)
+
+
+    def on_detection(self, message):
+        self.client.publish(self.topic, message)
+
     def start_sensing(self):
         while True:
             reply = input("Is there a car?")
             if reply == 'y':
-                print("Car, I saw a car!!")
+                self.on_detection("Car, I saw a car!!")
+
+
 
 if __name__ == '__main__':
     config = {'name': 'super sensor',
-              'location': 'L306'}
+              'location': 'L306',
+              'topic': "lot/sensor",
+              'broker': 'localhost',
+              'port': 1883}
+
     sensor = Sensor(config)
+    print("Sensor initialized")
+    sensor.start_sensing()
