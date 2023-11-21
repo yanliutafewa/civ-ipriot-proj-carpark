48f6e358396bf5228f07adb0aabc076a8fbafa59
yanliutafewa <141901817+yanliutafewa@users.noreply.github.com>
2023-11-21 21:56:33 +0800

    Completed 3 main requirements. (#2)
    * complete-update-temperature
    
    * backup
    
    * daily backup
    
    * All Done

--- src/car_park.py
+++ src/car_park.py
@@ -1,8 +1,164 @@
-class CarPark:
-    def __init__(self, car_park_id, car_park_name, car_park_address, temperature, parking_bays, occupied):
+import tkinter as tk
+from publisher import Publisher
+from tkinter import messagebox
+import paho.mqtt.client as mqtt
+import time
+import threading
+
+
+class CarParkInfo:
+    def __init__(self, car_park_id, car_park_name, car_park_address, temperature, parking_bays,
+                 occupied):
         self.car_park_id = car_park_id
         self.car_park_name = car_park_name
         self.car_park_address = car_park_address
         self.temperature = temperature
         self.parking_bays = parking_bays
         self.occupied = occupied
+
+
+class CarPark:
+    def __init__(self, displayer, car_park_info):
+
+        self.car_park_info = car_park_info
+
+        displayer.geometry('600x400')
+        displayer.title(car_park_info.car_park_name)
+
+        # set title
+        l_title = tk.Label(displayer, text=self.car_park_info.car_park_name, font=('Arial', 20))
+        l_title.grid(row=0, column=0, columnspan=3, pady=(20, 5))
+
+        # set car park info
+        # row for car_park_id
+        l_11 = tk.Label(displayer, text="car_park_id :", font=('Arial', 14))
+        l_11.grid(row=1, column=0, sticky="W", pady=(20, 5), padx=(50, 5))
+        l_21 = tk.Label(displayer, text=f"{self.car_park_info.car_park_id}", font=('Arial', 14))
+        l_21.grid(row=1, column=1, sticky="W", pady=(20, 5))
+
+        # row for car_park_address
+        l_12 = tk.Label(displayer, text="car_park_address :", font=('Arial', 14))
+        l_12.grid(row=2, column=0, sticky="W", pady=5, padx=(50, 5))
+        l_22 = tk.Label(displayer, text=f"{self.car_park_info.car_park_address}", font=('Arial', 14))
+        l_22.grid(row=2, column=1, sticky="W", pady=5)
+
+        # row for parking_bays
+        l_13 = tk.Label(displayer, text="parking_bays :", font=('Arial', 14))
+        l_13.grid(row=3, column=0, sticky="W", pady=5, padx=(50, 5))
+        l_23 = tk.Label(displayer, text=f"{self.car_park_info.parking_bays}", font=('Arial', 14))
+        l_23.grid(row=3, column=1, sticky="W", pady=5)
+
+        # row for occupied
+        l_14 = tk.Label(displayer, text="occupied :", font=('Arial', 14))
+        l_14.grid(row=4, column=0, sticky="W", pady=5, padx=(50, 5))
+        self.l_occupied = tk.Label(displayer, text=f"{self.car_park_info.occupied}", font=('Arial', 14))
+        self.l_occupied.grid(row=4, column=1, sticky="W", pady=5)
+
+        # row for temperature
+        l_15 = tk.Label(displayer, text="temperature :", font=('Arial', 14))
+        l_15.grid(row=5, column=0, sticky="W", pady=5, padx=(50, 5))
+
+        # create a frame to store the entry control and ℃ label
+        frame = tk.Frame(displayer)
+        frame.grid(row=5,  column=1, sticky="W", pady=5)
+        e_temperature = tk.Entry(frame, width=5, font=('Arial', 14))
+        e_temperature.insert(0, self.car_park_info.temperature)
+        e_temperature.pack(side=tk.LEFT)
+        l_25 = tk.Label(frame, text="℃", font=('Arial', 14))
+        l_25.pack(side=tk.LEFT)
+
+        # 'update temperature' button
+        def update_temperature():
+            new_temperature = e_temperature.get()
+            # validation
+            if not new_temperature.lstrip('-+').isdigit():
+                messagebox.showinfo("Error",
+                                    f"Please input a digital number for the temperature.")
+                return
+
+            self.car_park_info.temperature = new_temperature
+            messagebox.showinfo("Message",
+                                f"Temperature has been set as {self.car_park_info.temperature}℃ "
+                                f"for Car Park {self.car_park_info.car_park_id}.")
+        b_entry = tk.Button(displayer, text="Update Temperature", command=update_temperature, font=('Arial', 14))
+        b_entry.grid(row=5, column=2, sticky="W", pady=5)
+
+        # add MQTT publisher for transfer car park info every 3 second.
+        self.publisher_car_park_info = Publisher("CarParkInfo")
+
+        # Start the MQTT loop in a separate thread
+        mqtt_thread = threading.Thread(target=self.publish_loop)
+        mqtt_thread.daemon = True  # Daemonize the thread to stop when the main program exits
+        mqtt_thread.start()
+
+        # add MQTT subscriber for Car-IN Car-OUT
+        self.client = mqtt.Client()
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+        # Connect to MQTT broker
+        self.client.connect("localhost", 1883, 600)
+        # Start MQTT loop in a non-blocking way
+        self.client.loop_start()
+
+    def publish_loop(self):
+        while True:
+            # Publish a message to a specific topic
+            message = (f"{self.car_park_info.car_park_id},"
+                       f"{self.car_park_info.car_park_name},"
+                       f"{self.car_park_info.car_park_address},"
+                       f"{self.car_park_info.temperature}, "
+                       f"{self.car_park_info.parking_bays},"
+                       f"{self.car_park_info.occupied}")
+            self.publisher_car_park_info.publish_msg(message)
+            print(f"Message published: {message}")
+            time.sleep(2)  # Sleep for 10 seconds
+
+    def on_connect(self, client, userdata, flags, rc):
+        print("Connected with result code " + str(rc))
+        # Subscribe to a topic upon successful connection
+        client.subscribe("CarAct")
+
+    def on_message(self, client, userdata, msg):
+        message = msg.payload.decode()
+        print("Received message: " + message)
+
+        # get effective info
+        car_park_id = str(message).split(",")[0]
+        act = str(message).split(",")[1]
+
+        # update occupied number for corresponding car park
+        if car_park_id == self.car_park_info.car_park_id:
+            if act == 'in':
+                self.car_park_info.occupied += 1
+            if act == 'out':
+                self.car_park_info.occupied -= 1
+            self.l_occupied.config(text=self.car_park_info.occupied)
+
+
+def main():
+    car_park_1 = CarParkInfo("1", "Wilson Parking",
+                             "102 Wilson Street", 25,
+                             20, 10)
+    car_park_2 = CarParkInfo("2", "QV1 Car Park",
+                             "51 Murry Street", 20,
+                             28, 2)
+    car_park_3 = CarParkInfo("3", "CPP Car Park",
+                             "87-89 Pier Str", 21,
+                             35, 30)
+
+    # displayer_1 = tk.Tk()
+    # CarPark(displayer_1, car_park_1)
+
+    displayer_2 = tk.Tk()
+    CarPark(displayer_2, car_park_2)
+
+    # displayer_3 = tk.Tk()
+    # CarPark(displayer_3, car_park_3)
+
+    # displayer_1.mainloop()
+    displayer_2.mainloop()
+    # displayer_3.mainloop()
+
+
+if __name__ == "__main__":
+    main()
--- src/car_parks_center.py
+++ src/car_parks_center.py
@@ -1,173 +1,111 @@
 import tkinter as tk
+from tkinter import ttk
 import paho.mqtt.client as mqtt
-from car_park import CarPark
 
 
 class CarParksCenter:
 
-    def __init__(self):
+    # car_park_list = list()
 
-        self.car_park_1 = CarPark("1", "Wilson Parking", "102 Wilson Street", 25, 20, 10)
-        self.car_park_2 = CarPark("2", "QV1 Car Park", "51 Murry Street", 20, 28, 2)
-        self.car_park_3 = CarPark("3", "CPP Car Park", "87-89 Pier Str", 21, 35, 30)
+    def __init__(self, displayer):
 
-        self.displayer = tk.Tk()
-        self.displayer.geometry('1200x400')
-        self.displayer.title("Moondalup CarPark Management Center")
+        displayer.geometry('1200x400')
+        displayer.title("Moondalup CarPark Management Center")
 
-        l_title = tk.Label(self.displayer, text='Moondalup CarPark Management Center Monitor',font=('Arial', 28))
-        l_title.grid(row=0, column=0, columnspan=6, pady=(20,5))
+        l_title = tk.Label(displayer, text='Moondalup CarPark Management Center Monitor',
+                           font=('Arial', 28))
+        l_title.pack()
 
-        # set for car_park_1
-        # set Properties' name
-        l_11 = tk.Label(self.displayer, text="car_park_id :", font=('Arial', 14))
-        l_12 = tk.Label(self.displayer, text="car_park_name :", font=('Arial', 14))
-        l_13 = tk.Label(self.displayer, text="car_park_address :", font=('Arial', 14))
-        l_14 = tk.Label(self.displayer, text="temperature :", font=('Arial', 14))
-        l_15 = tk.Label(self.displayer, text="parking_bays :", font=('Arial', 14))
-        l_16 = tk.Label(self.displayer, text="occupied :", font=('Arial', 14))
+        style = ttk.Style()
+        style.configure("Treeview.Heading", font=('Calibri', 14, 'bold'))
+        style.configure("Treeview", font=('Calibri', 12))
 
-        l_11.grid(row=1, column=0, sticky="W", pady=(20,5), padx=(50, 5))
-        l_12.grid(row=2, column=0, sticky="W", pady=5, padx=(50, 5))
-        l_13.grid(row=3, column=0, sticky="W", pady=5, padx=(50, 5))
-        l_14.grid(row=4, column=0, sticky="W", pady=5, padx=(50, 5))
-        l_15.grid(row=5, column=0, sticky="W", pady=5, padx=(50, 5))
-        l_16.grid(row=6, column=0, sticky="W", pady=5, padx=(50, 5))
-
-        # set values
-        l_21 = tk.Label(self.displayer, text=f"{self.car_park_1.car_park_id}", font=('Arial', 14))
-        l_22 = tk.Label(self.displayer, text=f"{self.car_park_1.car_park_name}", font=('Arial', 14))
-        l_23 = tk.Label(self.displayer, text=f"{self.car_park_1.car_park_address}", font=('Arial', 14))
-        l_24 = tk.Label(self.displayer, text=f"{self.car_park_1.temperature}℃", font=('Arial', 14))
-        l_25 = tk.Label(self.displayer, text=f"{self.car_park_1.parking_bays}", font=('Arial', 14))
-        self.l_26 = tk.Label(self.displayer, text=f"{self.car_park_1.occupied}", font=('Arial', 14))
-
-        # this will arrange entry widgets
-        l_21.grid(row=1, column=1, sticky="W", pady=(20,5))
-        l_22.grid(row=2, column=1, sticky="W", pady=5)
-        l_23.grid(row=3, column=1, sticky="W", pady=5)
-        l_24.grid(row=4, column=1, sticky="W", pady=5)
-        l_25.grid(row=5, column=1, sticky="W", pady=5)
-        self.l_26.grid(row=6, column=1, sticky="W", pady=5)
-
-        # set for car_park_2
-        # set Properties' name
-        l_31 = tk.Label(self.displayer, text="car_park_id :", font=('Arial', 14))
-        l_32 = tk.Label(self.displayer, text="car_park_name :", font=('Arial', 14))
-        l_33 = tk.Label(self.displayer, text="car_park_address :", font=('Arial', 14))
-        l_34 = tk.Label(self.displayer, text="temperature :", font=('Arial', 14))
-        l_35 = tk.Label(self.displayer, text="parking_bays :", font=('Arial', 14))
-        l_36 = tk.Label(self.displayer, text="occupied :", font=('Arial', 14))
-
-        l_31.grid(row=1, column=2, sticky="W", pady=(20,5), padx=(60, 5))
-        l_32.grid(row=2, column=2, sticky="W", pady=5, padx=(60, 5))
-        l_33.grid(row=3, column=2, sticky="W", pady=5, padx=(60, 5))
-        l_34.grid(row=4, column=2, sticky="W", pady=5, padx=(60, 5))
-        l_35.grid(row=5, column=2, sticky="W", pady=5, padx=(60, 5))
-        l_36.grid(row=6, column=2, sticky="W", pady=5, padx=(60, 5))
-
-        # set values
-        l_41 = tk.Label(self.displayer, text=f"{self.car_park_2.car_park_id}", font=('Arial', 14))
-        l_42 = tk.Label(self.displayer, text=f"{self.car_park_2.car_park_name}", font=('Arial', 14))
-        l_43 = tk.Label(self.displayer, text=f"{self.car_park_2.car_park_address}", font=('Arial', 14))
-        l_44 = tk.Label(self.displayer, text=f"{self.car_park_2.temperature}℃", font=('Arial', 14))
-        l_45 = tk.Label(self.displayer, text=f"{self.car_park_2.parking_bays}", font=('Arial', 14))
-        self.l_46 = tk.Label(self.displayer, text=f"{self.car_park_2.occupied}", font=('Arial', 14))
-
-        # this will arrange entry widgets
-        l_41.grid(row=1, column=3, sticky="W", pady=(20,5))
-        l_42.grid(row=2, column=3, sticky="W", pady=5)
-        l_43.grid(row=3, column=3, sticky="W", pady=5)
-        l_44.grid(row=4, column=3, sticky="W", pady=5)
-        l_45.grid(row=5, column=3, sticky="W", pady=5)
-        self.l_46.grid(row=6, column=3, sticky="W", pady=5)
-
-        # set for car_park_3
-        # set Properties' name
-        l_51 = tk.Label(self.displayer, text="car_park_id :", font=('Arial', 14))
-        l_52 = tk.Label(self.displayer, text="car_park_name :", font=('Arial', 14))
-        l_53 = tk.Label(self.displayer, text="car_park_address :", font=('Arial', 14))
-        l_54 = tk.Label(self.displayer, text="temperature :", font=('Arial', 14))
-        l_55 = tk.Label(self.displayer, text="parking_bays :", font=('Arial', 14))
-        l_56 = tk.Label(self.displayer, text="occupied :", font=('Arial', 14))
-
-        l_51.grid(row=1, column=4, sticky="W", pady=(20,5), padx=(60, 5))
-        l_52.grid(row=2, column=4, sticky="W", pady=5, padx=(60, 5))
-        l_53.grid(row=3, column=4, sticky="W", pady=5, padx=(60, 5))
-        l_54.grid(row=4, column=4, sticky="W", pady=5, padx=(60, 5))
-        l_55.grid(row=5, column=4, sticky="W", pady=5, padx=(60, 5))
-        l_56.grid(row=6, column=4, sticky="W", pady=5, padx=(60, 5))
-
-        # set values
-        l_61 = tk.Label(self.displayer, text=f"{self.car_park_3.car_park_id}", font=('Arial', 14))
-        l_62 = tk.Label(self.displayer, text=f"{self.car_park_3.car_park_name}", font=('Arial', 14))
-        l_63 = tk.Label(self.displayer, text=f"{self.car_park_3.car_park_address}", font=('Arial', 14))
-        l_64 = tk.Label(self.displayer, text=f"{self.car_park_3.temperature}℃", font=('Arial', 14))
-        l_65 = tk.Label(self.displayer, text=f"{self.car_park_3.parking_bays}", font=('Arial', 14))
-        self.l_66 = tk.Label(self.displayer, text=f"{self.car_park_3.occupied}", font=('Arial', 14))
-
-        # this will arrange entry widgets
-        l_61.grid(row=1, column=5, sticky="W", pady=(20,5))
-        l_62.grid(row=2, column=5, sticky="W", pady=5)
-        l_63.grid(row=3, column=5, sticky="W", pady=5)
-        l_64.grid(row=4, column=5, sticky="W", pady=5)
-        l_65.grid(row=5, column=5, sticky="W", pady=5)
-        self.l_66.grid(row=6, column=5, sticky="W", pady=5)
-
-        self.label = tk.Label(self.displayer, text="Messages from MQTT will appear here:")
-        self.label.grid(row=7, column=0, columnspan=6, pady=(20,5))
-
+        # Create Treeview widget
+        self.tv_car_parks = ttk.Treeview(displayer)
+        self.tv_car_parks["columns"] = ("car park id", "car park name", "address", "temperature", "parking bays",
+                                        "occupied")
+        self.tv_car_parks.column("car park id", width=50, anchor=tk.CENTER)
+        self.tv_car_parks.column("car park name", width=50, anchor=tk.CENTER)
+        self.tv_car_parks.column("address", width=50, anchor=tk.CENTER)
+        self.tv_car_parks.column("temperature", width=50, anchor=tk.CENTER)
+        self.tv_car_parks.column("parking bays", width=50, anchor=tk.CENTER)
+        self.tv_car_parks.column("occupied", width=50, anchor=tk.CENTER)
+        self.tv_car_parks.heading("car park id", text="car park id")
+        self.tv_car_parks.heading("car park name", text="car park name")
+        self.tv_car_parks.heading("address", text="address")
+        self.tv_car_parks.heading("temperature", text="temperature")
+        self.tv_car_parks.heading("parking bays", text="parking bays")
+        self.tv_car_parks.heading("occupied", text="occupied")
+        self.tv_car_parks.pack(expand=True, fill=tk.BOTH)
+        self.tv_car_parks['show'] = 'headings'
 
         # Initialize MQTT subscriber
         self.client = mqtt.Client()
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
-
         # Connect to MQTT broker
-        self.client.connect("localhost", 1883, 600)
-
+        self.client.connect("127.0.0.1", 1883, 600)
         # Start MQTT loop in a non-blocking way
         self.client.loop_start()
 
     def on_connect(self, client, userdata, flags, rc):
         print("Connected with result code " + str(rc))
-        # Subscribe to a topic upon successful connection
+        # Subscribe to topics
         client.subscribe("CarAct")
+        client.subscribe("CarParkInfo")
 
     def on_message(self, client, userdata, msg):
         message = msg.payload.decode()
-        print("Received message: " + message)
+        topic = msg.topic
+        print("topic:" + topic + ",Received message: " + message)
+        # get car_park_id from message
+        car_park_id = str(message).split(",")[0]
 
-        # get effective info
-        act = str(message).split(",")[0]
-        car_park_id = str(message).split(",")[1]
+        if topic == 'CarParkInfo':
+            for line in self.tv_car_parks.get_children():
+                if self.tv_car_parks.item(line)['values'][0] == int(car_park_id):
+                    self.tv_car_parks.item(line, values=(self.tv_car_parks.item(line)['values'][0],
+                                                         self.tv_car_parks.item(line)['values'][1],
+                                                         self.tv_car_parks.item(line)['values'][2],
+                                                         str(message).split(",")[3] + "℃",
+                                                         self.tv_car_parks.item(line)['values'][4],
+                                                         str(message).split(",")[5]))
+                    return
+            # if not existed in treeview tv_car_parks, insert
+            self.tv_car_parks.insert("", "end",
+                                     values=(str(message).split(",")[0], str(message).split(",")[1],
+                                             str(message).split(",")[2], str(message).split(",")[3] + "℃",
+                                             str(message).split(",")[4], str(message).split(",")[5]))
+            return
+        if topic == 'CarAct':
+            for line in self.tv_car_parks.get_children():
+                if self.tv_car_parks.item(line)['values'][0] == int(car_park_id):
+                    # get action 'in/out' from message
+                    act = str(message).split(",")[1]
+                    # get parking_bays and occupied from tree item.
+                    parking_bays = self.tv_car_parks.item(line)['values'][4]
+                    occupied = self.tv_car_parks.item(line)['values'][5]
+                    if act == 'in' and occupied < parking_bays:
+                        self.tv_car_parks.item(line, values=(self.tv_car_parks.item(line)['values'][0],
+                                               self.tv_car_parks.item(line)['values'][1],
+                                               self.tv_car_parks.item(line)['values'][2],
+                                               self.tv_car_parks.item(line)['values'][3],
+                                               parking_bays,
+                                               occupied+1))
+                    if act == 'out' and occupied > 0:
+                        self.tv_car_parks.item(line, values=(self.tv_car_parks.item(line)['values'][0],
+                                               self.tv_car_parks.item(line)['values'][1],
+                                               self.tv_car_parks.item(line)['values'][2],
+                                               self.tv_car_parks.item(line)['values'][3],
+                                               parking_bays,
+                                               occupied-1))
 
-        # update occupied number for corresponding car park
-        if act == 'in' and car_park_id == '1':
-            self.car_park_1.occupied += 1
-            self.l_26.config(text=self.car_park_1.occupied)
-        if act == 'out' and car_park_id == '1':
-            self.car_park_1.occupied -= 1
-            self.l_26.config(text=self.car_park_1.occupied)
 
-        if act == 'in' and car_park_id == '2':
-            self.car_park_2.occupied += 1
-            self.l_46.config(text=self.car_park_2.occupied)
-        if act == 'out' and car_park_id == '2':
-            self.car_park_2.occupied -= 1
-            self.l_46.config(text=self.car_park_2.occupied)
-
-        if act == 'in' and car_park_id == '3':
-            self.car_park_3.occupied += 1
-            self.l_66.config(text=self.car_park_3.occupied)
-        if act == 'out' and car_park_id == '3':
-            self.car_park_3.occupied -= 1
-            self.l_66.config(text=self.car_park_3.occupied)
-
-    def run(self):
-        self.displayer.mainloop()
+def main():
+    displayer = tk.Tk()
+    CarParksCenter(displayer)
+    displayer.mainloop()
 
 
 if __name__ == "__main__":
-    app = CarParksCenter()
-    app.run()
+    main()
